1a5613580302e7e9481782a086676eb1e7280f26
bartlomiej.szejna@gmail.com <bartlomiej.szejna@gmail.com@33cf8874-a592-7a07-18e6-92596ed703ef>
2011-01-23 19:09:51 +0000

    git-svn-id: https://mosig1-os-nachos.googlecode.com/svn/trunk@27 33cf8874-a592-7a07-18e6-92596ed703ef

---
+++ code/userprog/frameprovider.cc
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+#include "frameprovider.h"
+
+FrameProvider::FrameProvider(int numPages) {
+    this->numPages=numPages;
+    frameBitMap = new BitMap(this->numPages);
+}
+
+int FrameProvider::GetEmptyframe() {
+    int randomFrame;
+    if (this->NumAvailFrame()==0) return -1;
+    do{
+    randomFrame =rand() % this->numPages;
+    }while(frameBitMap->Test(randomFrame));
+    frameBitMap->Mark(randomFrame);
+    return randomFrame;
+}
+
+void FrameProvider::ReleaseFrame(int which) {
+    frameBitMap->Clear(which);
+}
+
+int FrameProvider::NumAvailFrame() {
+    return frameBitMap->NumClear();
+}
+
+
