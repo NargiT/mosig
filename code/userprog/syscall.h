c4d0980a79c65511183ee61ae71b02b9ac6fd2e9
bartlomiej.szejna@gmail.com <bartlomiej.szejna@gmail.com@33cf8874-a592-7a07-18e6-92596ed703ef>
2011-01-24 17:51:21 +0000

    git-svn-id: https://mosig1-os-nachos.googlecode.com/svn/trunk@32 33cf8874-a592-7a07-18e6-92596ed703ef

--- code/userprog/syscall.h
+++ code/userprog/syscall.h
@@ -39,6 +39,7 @@
 #define SC_UserThreadCreate 17
 #define SC_UserThreadExit 18
 #define SC_UserThreadJoin 19
+#define SC_ForkExec 20
 #endif // CHANGED
 #ifdef IN_USER_MODE
 
@@ -161,6 +162,9 @@ int UserThreadCreate(void f(void *arg), void *arg);
 void UserThreadExit();
 
 void UserThreadJoin(int id_to_join);
+
+void ForkExec(char *ch);
+
 #endif // CHANGED
 
 #endif // IN_USER_MODE
