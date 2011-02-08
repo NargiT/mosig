95b8adc67dec8019ad227ada9587c6456bda102a
laurent.graebner@gmail.com <laurent.graebner@gmail.com@5cd0aab3-c614-fb2c-c540-a3fd922d460b>
2011-02-08 11:18:08 +0000

    git-svn-id: https://mosig1-ds-crazychat.googlecode.com/svn/trunk@9 5cd0aab3-c614-fb2c-c540-a3fd922d460b

--- src/chat/client/Client.java
+++ src/chat/client/Client.java
@@ -1,11 +1,34 @@
 package chat.client;
 
-public class Client {
+import chat.client.interfaces.ClientLocal;
+import chat.client.interfaces.ClientRemote;
+import chat.utils.Message;
 
-	boolean register() {
-		
-		
+public class Client implements ClientLocal, ClientRemote{
+
+	@Override
+	public void send(Message msg) {
+		// TODO Auto-generated method stub
 		
 	}
-	
+
+	@Override
+	public void register() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unregister() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receive(Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
--- src/chat/client/interfaces/ClientRemote.java
+++ src/chat/client/interfaces/ClientRemote.java
@@ -1,7 +1,9 @@
 package chat.client.interfaces;
 
+import chat.utils.Message;
+
 public interface ClientRemote {
 
-	
+	void receive(Message msg);
 	
 }
